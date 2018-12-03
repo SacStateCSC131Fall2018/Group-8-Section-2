507e2b0c37be69d8811b3bb932c489d9891fbce8
Balanay <balanay@csus.edu>
2018-12-03 09:28:55 -0800

    Menu Bar created (Correct Account used this time)

--- src/PirexWindow.java
+++ src/PirexWindow.java
@@ -12,6 +12,7 @@ public class PirexWindow extends JFrame {
     {
         super();
         setupLayout();
+        setupMenu();
         setSize(400,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
@@ -22,5 +23,42 @@ public class PirexWindow extends JFrame {
         add(new MainTabs(), BorderLayout.CENTER);
     }
     
-
+    private void setupMenu()
+    {
+    	setJMenuBar(createMenuBar());
+    }
+    
+    public JMenuBar createMenuBar()
+    {
+    	JMenuBar menuBar;
+    	JMenu menu;
+    	JMenuItem menuItem;
+    	
+    	//Creates the Menu Bar
+    	menuBar = new JMenuBar();
+    	
+    	//Creates the File Menu
+    	menu = new JMenu("File");
+        menu.getAccessibleContext().setAccessibleDescription("Dealing with Files");
+        menuBar.add(menu);
+        
+        //Save Query (Doesn't actually Save)
+        menuItem = new JMenuItem("Save Query");
+        menu.add(menuItem);
+        
+        //Load Query (Doesn't actually Load)
+        menuItem = new JMenuItem("Load Query");
+        menu.add(menuItem);
+        
+        //Export (Doesn't actually Export)
+        menuItem = new JMenuItem("Export");
+        menu.add(menuItem);
+        
+        //Exit (Actually Works)
+        menuItem = new JMenuItem("Exit");
+        menuItem.addActionListener((event) -> System.exit(0));
+        menu.add(menuItem);
+              
+    	return menuBar;
+    }
 }

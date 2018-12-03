3fe5f15f51c346dcd655f11bca357ca9804b66d5
s0fly <s0fly@10.0.0.108>
2018-12-03 09:24:45 -0800

    Used the wrong account to make changes

--- src/PirexWindow.java
+++ src/PirexWindow.java
@@ -23,42 +23,5 @@ public class PirexWindow extends JFrame {
         add(new MainTabs(), BorderLayout.CENTER);
     }
     
-    private void setupMenu()
-    {
-    	setJMenuBar(createMenuBar());
-    }
-    
-    public JMenuBar createMenuBar()
-    {
-    	JMenuBar menuBar;
-    	JMenu menu;
-    	JMenuItem menuItem;
-    	
-    	//Creates the Menu Bar
-    	menuBar = new JMenuBar();
-    	
-    	//Creates the File Menu
-    	menu = new JMenu("File");
-        menu.getAccessibleContext().setAccessibleDescription("Dealing with Files");
-        menuBar.add(menu);
-        
-        //Save Query (Doesn't actually Save)
-        menuItem = new JMenuItem("Save Query");
-        menu.add(menuItem);
-        
-        //Load Query (Doesn't actually Load)
-        menuItem = new JMenuItem("Load Query");
-        menu.add(menuItem);
-        
-        //Export (Doesn't actually Export)
-        menuItem = new JMenuItem("Export");
-        menu.add(menuItem);
-        
-        //Exit (Actually Works)
-        menuItem = new JMenuItem("Exit");
-        menuItem.addActionListener((event) -> System.exit(0));
-        menu.add(menuItem);
-              
-    	return menuBar;
-    }
+
 }
